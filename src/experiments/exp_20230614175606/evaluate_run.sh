33f2dfb0a27d43fab00c1e85e7d16a190800adb9
Upal Bhattacharya <upal.bhattacharya@gmail.com>
2023-06-20 10:47:44 +0530

    #85zt9d9k1 - Update paths for evaluation script.

--- src/experiments/exp_20230614175606/evaluate_run.sh
+++ src/experiments/exp_20230614175606/evaluate_run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-python evaluate.py -d /DATA/upal/Datasets/LLPE/variations/v1/data/test \
-    -t /DATA/upal/Datasets/LLPE/variations/v1/targets/case_statute_targets.json \
-    -n inlegalbert_r1_test \
-    -p params_word2vec_200.json \
+python evaluate.py -d /DATA1/upal/Datasets/LLPE/variations/v1/data/test \
+    -t /DATA1/upal/Datasets/LLPE/variations/v1/targets/case_statute_targets.json \
+    -n inlegalbert_r2_test \
+    -p params_inlegalbert.json \
     -lm "law-ai/InLegalBERT" \
-    -r /DATA/upal/Repos/llm-legal-prompt-engineering/exp_20230614175606/experiments/model_states/inlegalbert_r1/epoch_100.pth.tar \
+    -r /DATA1/upal/Repos/llm-legal-prompt-engineering/exp_20230614175606/experiments/model_states/inlegalbert_r1/epoch_26.pth.tar \
     -id 1
